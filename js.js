5f7483b897cbb1f67da10e34eef5bfde9cf91802
kawiba23 <122086056+kawiba23@users.noreply.github.com>
2023-02-11 14:30:28 +0100

    header changes and legal stuff

---
+++ js.js
@@ -0,0 +1,16 @@
+var app = angular.module('myApp', []);
+  app.controller('MyCtrl', function($scope) {
+});
+
+
+/*$( document ).ready(function() {
+    // Handler for .ready() called.
+    $("#collapseOne .btn").on("click", function(ev) {
+        console.log("clickery")
+        $.get("demo_test.asp", function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        })
+    });
+    
+  });
+  */
